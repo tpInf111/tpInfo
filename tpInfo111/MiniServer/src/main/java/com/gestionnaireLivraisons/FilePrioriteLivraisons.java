cfdfcb1efc4fb07e2ae38f7d09701e588cf06f6d
MelinaAit-Allah <aitmelina2018@gmail.com>
2026-03-02 14:52:01 -0500

    implementation de la class FilePrioriteLivraisons, question 1.9

--- tpInfo111/MiniServer/src/main/java/com/gestionnaireLivraisons/FilePrioriteLivraisons.java
+++ tpInfo111/MiniServer/src/main/java/com/gestionnaireLivraisons/FilePrioriteLivraisons.java
@@ -11,12 +11,14 @@ import java.util.PriorityQueue;
 public class FilePrioriteLivraisons {
     // Les livraisons stockées dans une file de priorité
     // TODO : À compléter/modifier
-
+    private PriorityQueue<Livraison> fileLivraisonPrio;
     /**
      * Construit une file de priorité.
      */
     // TODO : À compléter/modifier
-
+    public FilePrioriteLivraisons(){
+        this.fileLivraisonPrio = new PriorityQueue<>();
+    }
     /**
      * Retire et retourne l'élément le plus prioritaire de la file.
      *
@@ -24,7 +26,7 @@ public class FilePrioriteLivraisons {
      */
     public Livraison retirer() {
         // TODO : À compléter/modifier
-        return null;
+        return this.fileLivraisonPrio.poll();
     }
 
     /**
@@ -34,6 +36,7 @@ public class FilePrioriteLivraisons {
      */
     public void ajouter(Livraison element) {
         // TODO : À compléter/modifier
+        this.fileLivraisonPrio.add(element);
     }
 
     /**
@@ -43,6 +46,9 @@ public class FilePrioriteLivraisons {
      */
     public void ajouterTout(Iterable<Livraison> elements) {
         // TODO : À compléter/modifier
+        for(Livraison livraison :elements){
+            this.fileLivraisonPrio.add(livraison);
+        }
     }
 
     /**
@@ -52,7 +58,7 @@ public class FilePrioriteLivraisons {
      */
     public boolean estVide() {
         // TODO : À compléter/modifier
-        return false;
+        return this.fileLivraisonPrio.size()==0;
     }
 
     /**
@@ -62,7 +68,7 @@ public class FilePrioriteLivraisons {
      */
     public int taille() {
         // TODO : À compléter/modifier
-        return 0;
+        return this.fileLivraisonPrio.size();
     }
 
     /**
@@ -71,5 +77,8 @@ public class FilePrioriteLivraisons {
      */
     public void afficher() {
         // TODO : À compléter/modifier
+        for(Livraison livraison : this.fileLivraisonPrio){
+            System.out.print(livraison);
+        }
     }
 }
